984595115630778438b38cd230273b681b5c8648
constantp <constantp@slalom.com>
2018-01-26 07:32:45 -0800

    Initial Commit

---
+++ PhonewordBlank/IntStrToBoolStrConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace PhonewordBlank
+{
+    public class IntStrToBoolStrConverter : IValueConverter
+    {
+        public IntStrToBoolStrConverter()
+        {
+        }
+
+        // source to target
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int a = 5;
+
+            int val = Int32.Parse((string)value);
+            string param = (string)parameter;
+            int paramVal = Int32.Parse(param);
+
+            return (val + Triple(paramVal)) > 300;
+        }
+
+        // target to source
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int paramValue = Triple(Int32.Parse((string)parameter));
+            return (bool)value ? Math.Max(paramValue, 301) : 0;
+            // return val.ToString();
+        }
+
+        public int Triple(int param) {
+            return param * 3;
+        }
+    }
+}
---
+++ PhonewordBlank/ListingPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PhonewordBlank
+{
+    public partial class ListingPage : ContentPage
+    {
+        public List<Character> Characters { get; set; }
+
+        public ListingPage()
+        {
+            Characters = new List<Character> {
+                new Character {
+                    Name = "Name A",
+                    Class = "Warrior"
+                },
+                new Character {
+                    Name = "Name B",
+                    Class = "Archer"
+                }
+            };
+
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+
+    public class Character {
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+
+    public class ViewCellCustom : ViewCell
+    {
+        public ViewCellCustom()
+        {
+            //instantiate each of our views
+            StackLayout cellWrapper = new StackLayout();
+            StackLayout horizontalLayout = new StackLayout();
+            Label left = new Label();
+            Label right = new Label();
+
+            //set bindings
+            left.SetBinding(Label.TextProperty, "Name");
+            right.SetBinding(Label.TextProperty, "Class");
+
+            //Set properties for desired design
+            cellWrapper.BackgroundColor = Color.FromHex("#eee");
+            horizontalLayout.Orientation = StackOrientation.Horizontal;
+            right.HorizontalOptions = LayoutOptions.EndAndExpand;
+            left.TextColor = Color.FromHex("#f35e20");
+            right.TextColor = Color.FromHex("503026");
+
+            //add views to the view hierarchy
+            horizontalLayout.Children.Add(left);
+            horizontalLayout.Children.Add(right);
+            cellWrapper.Children.Add(horizontalLayout);
+            View = cellWrapper;
+        }
+    }
+}
